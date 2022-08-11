a87f6b3bdd113d3035bd02be70f7b5299ea59097
Ryan Speitel <rtspeitel@gmail.com>
2022-08-11 09:53:03 -0400

    Start some joplin configs, update the profile with more XDG notes, add dwmblocks cpu, update progs

--- suckless/dwmblocks/config.h
+++ suckless/dwmblocks/config.h
@@ -4,6 +4,6 @@
 static const Block blocks[] = {
 	{"~/arch/local/bin/statusbar/sb-mem", 60, 3},
 	{"~/arch/local/bin/statusbar/sb-ip", 360, 2},
-	{"~/arch/ocal/bin/statusbar/sb-vol", 360, 1},
+	{"~/arch/local/bin/statusbar/sb-vol", 360, 1},
 	{"~/arch/local/bin/statusbar/sb-clock", 5, 0},
 };
