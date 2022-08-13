a9cfc9348305382a25c7b76b74ba9f24a26932fc
Ryan Speitel <rtspeitel@gmail.com>
2022-08-12 22:40:26 -0400

    Update  spotify notifications, .gitignore, and remove some executables

--- suckless/dwmblocks/config.h
+++ suckless/dwmblocks/config.h
@@ -2,7 +2,7 @@
 #define delimiter " | "
 
 static const Block blocks[] = {
-	{"~/dotfiles/local/bin/statusbar/sb-cpu", 60, 3},
+	{"~/dotfiles/local/bin/statusbar/sb-cpu", 15, 3},
 	{"~/dotfiles/local/bin/statusbar/sb-mem", 60, 3},
 	{"~/dotfiles/local/bin/statusbar/sb-ip", 360, 2},
 	{"~/dotfiles/local/bin/statusbar/sb-vol", 360, 1},
